42baee86f4f31fe306b580b3db167ac96bb5b60f
Gabrel Leopoldino <gabrielperes97@gmail.com>
2018-10-10 19:39:52 -0300

    Initial commit. Communication between Mobj and Mhub working.

---
+++ src/main/java/gabrielleopoldino/contextnet/mobilehub/MobileHub.kt
@@ -0,0 +1,11 @@
+package gabrielleopoldino.contextnet.mobilehub
+
+import gabrielleopoldino.contextnet.mobilehub.tecnologies.simulator.SimulatorConnection
+
+object MobileHub {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        SimulatorConnection
+    }
+}
